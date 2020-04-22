4a0acb395897747dc3c7149219dd18a17151ea4c
Bechraoui <abechraoui@monceauassurances.com>
2020-04-22 18:24:37 +0200

    Repositories config and exposing IDs

--- springbootangular/src/main/java/tn/bechraoui/springbootangular/repository/CategoryRepository.java
+++ springbootangular/src/main/java/tn/bechraoui/springbootangular/repository/CategoryRepository.java
@@ -1,9 +1,11 @@
 package tn.bechraoui.springbootangular.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import tn.bechraoui.springbootangular.entity.Category;
 
+@RepositoryRestResource(collectionResourceRel="bookcategory")
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
